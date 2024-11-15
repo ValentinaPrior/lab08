508815b96a342d0055b5cd1c1b6942af673c630f
ValentinaPrior <vbp7@njit.edu>
2024-11-15 08:36:28 -0500

    QUESTION THREE

--- script.js
+++ script.js
@@ -6,5 +6,12 @@ console.log(name,age);
 // QUESTION TWO
 let color= "orange";
 let number= 14;
-
 console.log(color, number);
+
+//QUESTION THREE
+let RollNumber=15;
+if (RollNumber>10)
+{
+    let answer= "The RollNumber is greater than 10"
+    console.log(answer);
+}
